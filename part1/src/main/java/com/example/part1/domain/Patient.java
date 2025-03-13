38a25c7fd3a446f7dc47d159a3a618028bee6d5f
Steve Atchohe <sa1088.student.ac.uk>
2025-03-13 17:49:40 +0000

    Added the attributes to the patients domain class

--- part1/src/main/java/com/example/part1/domain/Patient.java
+++ part1/src/main/java/com/example/part1/domain/Patient.java
@@ -1,4 +1,70 @@
 package com.example.part1.domain;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String email;
+    String phoneNumber;
+    String address;
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, orphanRemoval = true)
+    List<Appointments> appointmentsList;
+
+
+    public Patient(Long id, String name, String email, String phoneNumber, String address){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+    public Patient() {}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
