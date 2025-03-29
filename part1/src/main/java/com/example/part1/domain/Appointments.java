bc3a324520559c3a24815e4ba5686253c0e2d568
DanStew <danielstewarts2004@gmail.com>
2025-03-29 14:59:47 +0000

    Reverting changes

--- part1/src/main/java/com/example/part1/domain/Appointments.java
+++ part1/src/main/java/com/example/part1/domain/Appointments.java
@@ -40,10 +40,6 @@ public class Appointments {
         this.medicalRecord = record;
     }
 
-    public Appointments() {
-
-    }
-
     //Getters and Setters for the attributes
     public long getId() {return id;}
     public void setId(long id) {this.id = id;}
